ee018ea097ca1a1c10431b244afac2857b294856
Mega Master <megaandre011@gmail.com>
2022-04-18 20:58:02 +0200

    18.04.2022

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,17 @@
     <body>
         Тут я буду писать код PHP <br>
 
-        <?php 
-        $main = "Andrey";
-        if ($main === "Andrey")
-            echo "Добро пожаловать $main"
+        <?php
+        $arr[0] = "PHP"; 
+        $arr[1] = "HTML"; 
+        $arr[2] = "CSS";
+
+        $arr[1] = "JAVASCRIPT";
+        $arr[]  = "JQUERY";
+
+        foreach($arr as $key => $value) { // при переборе: $key - индекс элемента массива, $value - значение элемента массива
+            echo $value.'<br/>';
+        }
         ?>
     </body>
 </html>
